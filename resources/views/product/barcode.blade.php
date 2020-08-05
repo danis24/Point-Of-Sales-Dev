883816c74a61a14c5c45d834dd3f24427890637b
Danis Yogaswara <danis24@Daniss-MacBook-Air.local>
2020-08-05 15:38:30 +0700

    add margin in page

--- resources/views/product/barcode.blade.php
+++ resources/views/product/barcode.blade.php
@@ -2,15 +2,19 @@
 <html>
 <head>
 	<title>Cetak Barcode</title>
+	<style>
+		@page { margin: 0px; }
+		body { margin: 0px; }
+	</style>
 </head>
 <body>
-	<table width="100%">
+	<table width="100%" style="border-spacing: 2mm;">
 		<tr>
 			@foreach($data_product as $data)
 			<td align="center" style="border: 1px solid #ccc">
-				<span style="font-size: 8">{{$data['product_name']}}</span><br>
+				<span style="font-size: 5">{{$data['product_name']}}</span><br>
 				<img src="data:image/png;base64,{{$data['barcode']}}" alt="barcode"/><br>
-				<span style="font-size: 8">{{$data['product_code']}}</span>
+				<span style="font-size: 5">{{$data['product_code']}}</span>
 			</td>
 			@if($no++ % 5 == 0)
 		</tr>
