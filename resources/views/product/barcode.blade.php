0568f4471ade3603b52f9eed6355a2ef3ee23267
Danis Yogaswara <danis24@Daniss-MacBook-Air.local>
2020-08-10 15:27:54 +0700

    fix layout

--- resources/views/product/barcode.blade.php
+++ resources/views/product/barcode.blade.php
@@ -3,15 +3,15 @@
 <head>
 	<title>Cetak Barcode</title>
 	<style>
-		@page { margin-top: 0mm; margin-bottom: 5mm; margin-left: -1mm; margin-right: 2mm}
+		@page { margin-top: -2mm; margin-bottom: 5mm; margin-left: -2mm; margin-right: 2mm}
 		body { margin: 0px; }
 	</style>
 </head>
 <body>
-	<table style="border-spacing: 3mm 3mm">
+	<table style="border-spacing: 10px 17px">
 		<tr>
 			@foreach($data_product as $data)
-			<td align="center" style="width: 36mm; height: 16mm">
+			<td align="center" style="border: 1px solid #ccc; width: 35mm; height: 15mm">
 				<span style="font-size: 4">{{$data['product_name']}}</span><br>
 				<img src="data:image/png;base64,{{$data['barcode']}}" alt="barcode"/><br>
 				<span style="font-size: 5">{{$data['product_code']}}</span>
